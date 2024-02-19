c08714178b974044a5eb2908b98dce638c869d15
beaudelaire <Beaudelaire.dev@gmail.com>
2024-02-19 12:45:37 -0500

    added course reviews on ui

---
+++ ui/src/lib/components/Course/Review/IconRatingInput.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import {Flame, UserRound} from "lucide-svelte";
+    import type {Writable} from "svelte/store";
+
+    export let icon: 'flame' | 'user';
+    export let rating: number;
+    export let name: string;
+    export let fieldValue: Writable<number>;
+</script>
+
+    {#each Array(5) as _, i }
+        <button class='cursor-pointer' on:click={() => fieldValue.set(i + 1)}>
+        {#if icon === 'flame' }
+            <Flame class={i < rating ? 'fill-blue-600 ' : 'fill-gray-200'} size={22} strokeWidth={0} id={`${name}-star-${i}`}/>
+        {:else }
+            <UserRound class={i < rating ? 'fill-blue-600 ' : 'fill-gray-200'} size={22} strokeWidth={0} id={`${name}-star-${i}`}/>
+        {/if}
+            </button>
+    {/each}
---
+++ ui/src/lib/components/Course/Review/ReviewForm.svelte
@@ -0,0 +1,90 @@
+<script lang="ts">
+    import type {Course} from "$lib/model/Course";
+    import MultiSelect from "$lib/components/Explore/MultiSelect.svelte";
+    import {Field} from "sveltik/src";
+    import FieldLabel from "$lib/components/Course/Review/FieldLabel.svelte";
+    import FieldError from "$lib/components/Course/Review/FieldError.svelte";
+    import IconRatingInput from "$lib/components/Course/Review/IconRatingInput.svelte";
+    import {writable} from "svelte/store";
+
+    export let course: Course;
+    export let setFieldValue: (
+        field: string,
+        value: any,
+        shouldValidate?: boolean | undefined
+    ) => void;
+    export let values: ReviewFormInitialValues;
+    export let resetForm: () => void;
+
+    const instructorNames = Array.from(
+        new Set(course.instructors.map((instructor) => instructor.name))
+    );
+
+    instructorNames.push('Other');
+    type ReviewFormInitialValues = {
+        content: string;
+        instructors: string[];
+        rating: number;
+        difficulty: number;
+    };
+    const instructors = writable(values.instructors);
+    const rating = writable(values.rating);
+    const difficulty = writable(values.difficulty);
+</script>
+
+<FieldLabel For='instructors'>Instructor(s)</FieldLabel>
+<MultiSelect
+        className='mt-2'
+        inputClassName='bg-neutral-100'
+        options={instructorNames}
+        values={instructors}
+/>
+<FieldError name='instructors'/>
+<div class='flex gap-x-10'>
+    <div class='flex flex-col gap-y-1'>
+        <FieldLabel For='rating'>Rating</FieldLabel>
+        <IconRatingInput
+                name='rating'
+                rating={values.rating}
+                icon='user'
+                fieldValue={rating}
+        />
+    </div>
+    <div class='flex flex-col gap-y-0.5'>
+        <FieldLabel For='difficulty'>Difficulty</FieldLabel>
+        <IconRatingInput
+                name='difficulty'
+                rating={values.difficulty}
+                icon='flame'
+                fieldValue={difficulty}
+        />
+    </div>
+</div>
+<FieldError name='rating'/>
+<FieldError name='difficulty'/>
+<div class='py-1'/>
+<div class='flex flex-col'>
+    <FieldLabel For='content'>Content</FieldLabel>
+    <Field
+            component='textarea'
+            rows='8'
+            id='content'
+            name='content'
+            placeholder='Write your thoughts on this course...'
+            class='resize-none rounded-md border bg-gray-50 p-3 outline-none dark:border-neutral-600 dark:bg-neutral-700 dark:text-gray-200 dark:caret-white'
+    />
+    <FieldError name='content'/>
+    <div class='mt-8 flex justify-end space-x-4'>
+        <button on:click={() => resetForm()}
+                class='w-fit cursor-pointer rounded-md bg-gray-100 px-4 py-2 font-medium text-gray-700 duration-200 hover:bg-gray-200 dark:bg-neutral-700 dark:text-gray-200 dark:hover:bg-neutral-600'
+        >
+            Discard
+        </button>
+        <button
+                type='submit'
+                class='ml-auto w-fit rounded-md bg-red-600 px-4 py-2 font-medium text-white transition duration-300 hover:bg-red-800'
+        >
+            Submit
+        </button>
+    </div>
+</div>
