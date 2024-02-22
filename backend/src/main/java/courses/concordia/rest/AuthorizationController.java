1fa2f8c3bc69c5e4c8abcf31785abcfd8699e7dc
ERO <zzhao260985652@gmail.com>
2024-02-21 22:28:09 -0500

    update

--- backend/src/main/java/courses/concordia/rest/AuthorizationController.java
+++ backend/src/main/java/courses/concordia/rest/AuthorizationController.java
@@ -1,27 +1,36 @@
 package courses.concordia.rest;
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/auth")
+import java.io.IOException;
+import java.util.Collections;
+import java.util.Map;
+
+
 @RestController
+@RequestMapping("/auth")
 public class AuthorizationController {
 
     @GetMapping("/authorized")
-    public String checkAuthorization(@AuthenticationPrincipal OAuth2User principal) {
-        if(principal != null){
-            return "user is authorized";
-        } else {
-            return "user is not authorized";
-        }
+    public Map<String, Object> authorize(@AuthenticationPrincipal OAuth2User principal) {
+        return Collections.singletonMap("name", principal.getAttribute("name"));
     }
 
     @GetMapping("/login")
-    public String login(){
-        return "redirecting to Microsoft";
+    public void Login(HttpServletResponse response) throws IOException {
+        String authorizationUri = "https://login.microsoftonline.com/common/oauth2/v2.0/authorize"
+                + "?client_id=fdb53ef1-b2d0-4f6c-afad-406a0d15bd41"
+                + "&response_type=code"
+                + "&redirect_uri=http://localhost:9191/"
+                + "&response_mode=query"
+                + "&scope=https://graph.microsoft.com/.default"
+                + "&state=12345";
+        response.sendRedirect(authorizationUri);
     }
 
     @GetMapping("/logout")
--- backend/src/main/java/courses/concordia/rest/SecurityConfig.java
+++ backend/src/main/java/courses/concordia/rest/SecurityConfig.java
@@ -1,3 +1,4 @@
+/*
 package courses.concordia.rest;
 
 
@@ -24,3 +25,4 @@ public class SecurityConfig{
                 .build();
     }
 }
+*/
