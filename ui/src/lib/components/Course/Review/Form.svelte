b226e2756d3dd49812671b4db402539589009cf9
beaudelaire <Beaudelaire.dev@gmail.com>
2024-02-29 11:10:35 -0500

    gitignore updated

--- ui/src/lib/components/Course/Review/Form.svelte
+++ ui/src/lib/components/Course/Review/Form.svelte
@@ -1,6 +1,6 @@
 <script>
     import { getContext } from 'svelte'
-    import { persist } from 'svelte-use-persist';
+    import { persist } from '$lib/persist';
 
     const handleSubmit = getContext('handleSubmit')
     const handleReset = getContext('handleReset')
