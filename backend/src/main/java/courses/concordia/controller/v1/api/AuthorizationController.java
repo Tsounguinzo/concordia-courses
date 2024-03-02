f8d50b937114fba6f1aacc5a9f9825f840d98cc1
ERO <zzhao260985652@gmail.com>
2024-03-02 16:01:49 -0500

    update

--- backend/src/main/java/courses/concordia/controller/v1/api/AuthorizationController.java
+++ backend/src/main/java/courses/concordia/controller/v1/api/AuthorizationController.java
@@ -1,39 +1,53 @@
 package courses.concordia.controller.v1.api;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import courses.concordia.dto.model.user.UserDto;
+import courses.concordia.service.implementation.EmailServiceImpl;
+import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.mail.MailException;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
 
-
-@RestController
+@Controller
 @RequestMapping("/api/v1/auth")
 public class AuthorizationController {
-    @GetMapping("/authorized")
-    public Map<String, String> authorize(@AuthenticationPrincipal OAuth2User principal) {
-        Map<String, String> map = new HashMap<>();
 
-        String email = principal.getAttribute("email");
-        if(email != null){
-            if(email.contains("concordia")){
-                map.put("name", principal.getAttribute("name"));
-                map.put("email",principal.getAttribute("email"));
-                //if the user is authorized then get name and email
-                return map;
-            }
+    @Autowired
+     EmailServiceImpl emailService;
+    private static final Logger LOG = LoggerFactory.getLogger(EmailController.class);
+    @PostMapping("/authorized")
+    public String processSignup(@ModelAttribute("user") @Valid UserDto userDto,
+                                BindingResult result) {
+        if (result.hasErrors()) {
+            return "signUpForm";
         }
-        //test
-        map.put("name", principal.getAttribute("name"));
-        map.put("email",principal.getAttribute("email"));
-        return map;
-
-        //return null;
+        try {
+            emailService.sendSimpleEmail(userDto.getEmail(), "Welcome", "This is a welcome email for your!!");
+        } catch (MailException mailException) {
+            LOG.error("Error while sending out email..{}", mailException.getStackTrace());
+            return "Unable to send email"+ HttpStatus.INTERNAL_SERVER_ERROR;
+        }
+        System.out.println(userDto);
+        return "index";
     }
+
+
+
+    @GetMapping("/signup")
+    public String showSignupForm(Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "signUpForm";
+    }
+
+
+
 }
 
