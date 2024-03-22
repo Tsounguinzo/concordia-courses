27608f8b09b9e668df5a027ed7a926a72a951ef2
beaudelaire <Beaudelaire.dev@gmail.com>
2024-03-22 18:59:59 -0400

    added config for mongo hosting, and bug fix

---
+++ backend/src/main/java/courses/concordia/config/MongoClientConfig.java
@@ -0,0 +1,32 @@
+package courses.concordia.config;
+
+import com.mongodb.ConnectionString;
+import com.mongodb.MongoClientSettings;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@Slf4j
+@EnableMongoAuditing //enable the automatic setting of @CreatedDate
+public class MongoClientConfig extends AbstractMongoClientConfiguration {
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+    @Value("${spring.data.mongodb.uri}")
+    private String connectionString;
+
+    @Override
+    protected String getDatabaseName() {
+        return database;
+    }
+
+    @Bean
+    public MongoClientSettings mongoClientSettings() {
+        log.info("=> Creating the MongoClientSettings for MongoClient & MongoTemplate.");
+        return MongoClientSettings.builder().applyConnectionString(new ConnectionString(connectionString)).build();
+    }
+}
