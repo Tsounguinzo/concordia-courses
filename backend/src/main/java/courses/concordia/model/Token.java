33e10a08fd04e0a5f1eb7b634d841137ad03445c
beaudelaire <Beaudelaire.dev@gmail.com>
2024-03-23 22:02:20 -0400

    refactor

--- backend/src/main/java/courses/concordia/model/Token.java
+++ backend/src/main/java/courses/concordia/model/Token.java
@@ -1,18 +1,11 @@
 package courses.concordia.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import lombok.experimental.Accessors;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import javax.persistence.FetchType;
-import javax.persistence.OneToOne;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
 import java.time.LocalDateTime;
 import java.util.Random;
 
@@ -20,6 +13,7 @@ import java.util.Random;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@ToString
 @Accessors(chain = true)
 @Document(collection = "tokens")
 public class Token {
--- ui/src/routes/+error.svelte
+++ ui/src/routes/+error.svelte
@@ -1,38 +1,27 @@
 <script>
     import {page} from "$app/stores";
+    import {Bug, MapPinned} from "lucide-svelte";
 </script>
 
 {#if ($page.status === 404) }
-    <div class="flex min-h-[calc(100vh-19vh)] flex-col items-center justify-center">
-        <div class='space-y-10 text-center justify-center'>
-            <div class="flex items-center max-w-[32rem] my-1">
-                <span class="text-5xl mr-1 font-bold text-gray-900 dark:text-gray-200">{$page.status}</span>
-                <div class="text-3xl border-l-2 pl-2 ml-1 border-black dark:border-gray-200 border-solid font-medium text-gray-800 dark:text-gray-300">
-                    <h1>It looks like you got lost.</h1>
-                </div>
-            </div>
-            <a href="/"
-               class='block mx-auto max-w-fit rounded-full border px-5 py-2 font-semibold text-gray-800 transition duration-300 ease-in-out hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-neutral-800'>
-                Home
-            </a>
-        </div>
+    <div class="flex min-h-[calc(100vh-19vh)] flex-col items-center justify-center text-center">
+        <MapPinned class="text-6xl text-gray-800 dark:text-gray-200 mb-4" />
+        <h1 class="text-4xl font-bold text-gray-900 dark:text-gray-200 mb-2">Page Not Found</h1>
+        <p class="mb-5 text-lg text-gray-600 dark:text-gray-400">We can't find the page you're looking for. It might have been removed or renamed, or maybe it never existed.</p>
+        <a href="/" class='inline-block rounded-lg bg-blue-500 px-6 py-3 text-lg font-semibold text-white transition duration-300 ease-in-out hover:bg-blue-600 dark:hover:bg-blue-700'>Go Home</a>
     </div>
 {:else }
 <div class='flex min-h-[calc(100vh-19vh)] items-center justify-center'>
-        <div class='text-center'>
-            <div class='text-xl text-gray-700 dark:text-gray-300'>
-              Something went wrong :(
-            </div>
-          <div class='py-3' />
-          <div class='text-sm dark:text-gray-200'>
-            Help us fix it by reporting this error with a screenshot on{' '}
-            <a
-              class='text-red-700 underline hover:text-red-800 dark:text-red-400 dark:hover:text-red-300'
-              href='https://github.com/Tsounguinzo/concordia-courses'
-            >
-              our GitHub
-            </a>
-          </div>
-        </div>
-      </div>
+    <div class='flex flex-col min-h-[calc(100vh-19vh)] items-center justify-center text-center'>
+            <Bug class="text-6xl text-red-500 dark:text-red-300 mb-4" />
+            <h2 class='text-3xl text-gray-700 dark:text-gray-300 mb-2'>Oops! Something Went Wrong.</h2>
+            <p class='mb-5 text-lg text-gray-600 dark:text-gray-400'>An unexpected error has occurred. We're working to fix the problem, so please try again later.</p>
+            <p class='text-sm text-gray-500 dark:text-gray-200'>
+                If you continue to experience issues, please let us know on{' '}
+                <a class='text-blue-500 underline hover:text-blue-600 dark:hover:text-blue-400' href='https://github.com/Tsounguinzo/concordia-courses'>
+                    our GitHub.
+                </a>
+            </p>
+    </div>
+</div>
 {/if}
