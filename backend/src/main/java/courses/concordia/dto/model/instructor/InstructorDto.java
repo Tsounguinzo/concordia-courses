15a0ed6d432dd48821d40f2a97caeffb1667295f
beaudelaire <Beaudelaire.dev@gmail.com>
2024-04-24 20:50:35 -0400

    added Instructor tags

--- backend/src/main/java/courses/concordia/dto/model/instructor/InstructorDto.java
+++ backend/src/main/java/courses/concordia/dto/model/instructor/InstructorDto.java
@@ -5,7 +5,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.*;
 import lombok.experimental.Accessors;
 
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -19,7 +19,8 @@ public class InstructorDto {
     private String firstName;
     private String lastName;
     private String department;
-    private List<InstructorCourseDto> courses;
+    private Set<InstructorCourseDto> courses;
+    private Set<InstructorTagDto> tags;
     @ToString.Exclude @EqualsAndHashCode.Exclude
     private double avgDifficulty = 0.0;
     @ToString.Exclude @EqualsAndHashCode.Exclude
@@ -32,5 +33,51 @@ public class InstructorDto {
     public static class InstructorCourseDto {
         private String subject;
         private String catalog;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum InstructorTagDto {
+        TOUGH_GRADER("Tough Grader"),
+        GET_READY_TO_READ("Get Ready To Read"),
+        PARTICIPATION_MATTERS("Participation Matters"),
+        EXTRA_CREDIT("Extra Credit"),
+        GROUP_PROJECTS("Group Projects"),
+        AMAZING_LECTURES("Amazing Lectures"),
+        CLEAR_GRADING_CRITERIA("Clear Grading Criteria"),
+        GIVES_GOOD_FEEDBACK("Gives Good Feedback"),
+        INSPIRATIONAL("Inspirational"),
+        LOTS_OF_HOMEWORK("Lots Of Homework"),
+        HILARIOUS("Hilarious"),
+        BEWARE_OF_POP_QUIZZES("Beware Of Pop Quizzes"),
+        SO_MANY_PAPERS("So Many Papers"),
+        CARING("Caring"),
+        RESPECTED("Respected"),
+        FLEXIBLE_DEADLINES("Flexible Deadlines"),
+        LECTURE_HEAVY("Lecture Heavy"),
+        TEST_HEAVY("Test Heavy"),
+        GRADED_BY_FEW_THINGS("Graded By Few Things"),
+        ACCESSIBLE_OUTSIDE_CLASS("Accessible Outside Class"),
+        ONLINE_SAVVY("Online Savvy"),
+        ENGAGING("Engaging"),
+        TECHNICALLY_PROFICIENT("Technically Proficient"),
+        INDUSTRY_EXPERIENCED("Industry Experienced"),
+        RESEARCH_ORIENTED("Research-Oriented"),
+        MULTIDISCIPLINARY_APPROACH("Multidisciplinary Approach"),
+        INTERACTIVE_SESSIONS("Interactive Sessions"),
+        ENCOURAGES_CRITICAL_THINKING("Encourages Critical Thinking"),
+        USES_MULTIMEDIA("Uses Multimedia"),
+        CULTURALLY_INCLUSIVE("Culturally Inclusive");
+
+        private final String displayName;
+
+        public static InstructorTagDto fromString(String text) {
+            for (InstructorTagDto b : InstructorTagDto.values()) {
+                if (b.displayName.equalsIgnoreCase(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
     }
 }
--- backend/src/main/java/courses/concordia/model/Instructor.java
+++ backend/src/main/java/courses/concordia/model/Instructor.java
@@ -5,7 +5,7 @@ import lombok.experimental.Accessors;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.util.List;
+import java.util.Set;
 
 @Getter
 @Setter
@@ -18,7 +18,8 @@ public class Instructor {
     private String firstName;
     private String lastName;
     private String department;
-    private List<InstructorCourse> courses;
+    private Set<InstructorCourse> courses;
+    private Set<InstructorTag> tags;
     @ToString.Exclude @EqualsAndHashCode.Exclude
     private double avgDifficulty = 0.0;
     @ToString.Exclude @EqualsAndHashCode.Exclude
@@ -31,5 +32,51 @@ public class Instructor {
     public static class InstructorCourse {
         private String subject;
         private String catalog;
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public enum InstructorTag {
+        TOUGH_GRADER("Tough Grader"),
+        GET_READY_TO_READ("Get Ready To Read"),
+        PARTICIPATION_MATTERS("Participation Matters"),
+        EXTRA_CREDIT("Extra Credit"),
+        GROUP_PROJECTS("Group Projects"),
+        AMAZING_LECTURES("Amazing Lectures"),
+        CLEAR_GRADING_CRITERIA("Clear Grading Criteria"),
+        GIVES_GOOD_FEEDBACK("Gives Good Feedback"),
+        INSPIRATIONAL("Inspirational"),
+        LOTS_OF_HOMEWORK("Lots Of Homework"),
+        HILARIOUS("Hilarious"),
+        BEWARE_OF_POP_QUIZZES("Beware Of Pop Quizzes"),
+        SO_MANY_PAPERS("So Many Papers"),
+        CARING("Caring"),
+        RESPECTED("Respected"),
+        FLEXIBLE_DEADLINES("Flexible Deadlines"),
+        LECTURE_HEAVY("Lecture Heavy"),
+        TEST_HEAVY("Test Heavy"),
+        GRADED_BY_FEW_THINGS("Graded By Few Things"),
+        ACCESSIBLE_OUTSIDE_CLASS("Accessible Outside Class"),
+        ONLINE_SAVVY("Online Savvy"),
+        ENGAGING("Engaging"),
+        TECHNICALLY_PROFICIENT("Technically Proficient"),
+        INDUSTRY_EXPERIENCED("Industry Experienced"),
+        RESEARCH_ORIENTED("Research-Oriented"),
+        MULTIDISCIPLINARY_APPROACH("Multidisciplinary Approach"),
+        INTERACTIVE_SESSIONS("Interactive Sessions"),
+        ENCOURAGES_CRITICAL_THINKING("Encourages Critical Thinking"),
+        USES_MULTIMEDIA("Uses Multimedia"),
+        CULTURALLY_INCLUSIVE("Culturally Inclusive");
+
+        private final String displayName;
+
+        public static InstructorTag fromString(String text) {
+            for (InstructorTag b : InstructorTag.values()) {
+                if (b.displayName.equalsIgnoreCase(text)) {
+                    return b;
+                }
+            }
+            return null;
+        }
     }
 }
