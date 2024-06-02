d7f341ed3ccce488ac2874df69b243f63b9eb6da
beaudelaire <Beaudelaire.dev@gmail.com>
2024-06-02 19:45:21 -0400

    fix: added host check

--- backend/src/main/java/courses/concordia/security/HostCheckFilter.java
+++ backend/src/main/java/courses/concordia/security/HostCheckFilter.java
@@ -4,24 +4,23 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
 @Component
-@Slf4j
 public class HostCheckFilter extends OncePerRequestFilter {
 
-    private static final String ALLOWED_HOST = "concordia.courses";
+    @Value("${allowed.host}")
+    private String ALLOWED_HOST;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
         String host = request.getHeader("Host");
 
-        log.info("Host: {}", host);
         if (ALLOWED_HOST.equals(host)) {
             filterChain.doFilter(request, response);
         } else {
