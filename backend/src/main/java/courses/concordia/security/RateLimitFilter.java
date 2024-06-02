269eb51894d4385c9103a454cf3c956cd16b6741
beaudelaire <Beaudelaire.dev@gmail.com>
2024-06-02 09:43:03 -0400

    fix: rate limit to reviews only

--- backend/src/main/java/courses/concordia/security/RateLimitFilter.java
+++ backend/src/main/java/courses/concordia/security/RateLimitFilter.java
@@ -15,20 +15,22 @@ import java.util.concurrent.TimeUnit;
 public class RateLimitFilter extends OncePerRequestFilter {
 
     private final ConcurrentHashMap<String, Long> requestCounts = new ConcurrentHashMap<>();
+    private static final String REVIEW_SUBMISSION_ENDPOINT = "/api/v1/reviews";
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
-        String clientIP = request.getRemoteAddr();
-        long currentTime = System.currentTimeMillis();
-        requestCounts.merge(clientIP, currentTime, (oldTime, newTime) -> {
-            if (newTime - oldTime < TimeUnit.SECONDS.toMillis(10)) {
-                response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-                return oldTime;
-            }
-            return newTime;
-        });
-
+        if ("POST".equalsIgnoreCase(request.getMethod()) && request.getRequestURI().startsWith(REVIEW_SUBMISSION_ENDPOINT)) {
+            String clientIP = request.getRemoteAddr();
+            long currentTime = System.currentTimeMillis();
+            requestCounts.merge(clientIP, currentTime, (oldTime, newTime) -> {
+                if (newTime - oldTime < TimeUnit.SECONDS.toMillis(10)) { // Limit to 1 request per 10 seconds
+                    response.setStatus(HttpServletResponse.SC_SERVICE_UNAVAILABLE);
+                    return oldTime;
+                }
+                return newTime;
+            });
+        }
         filterChain.doFilter(request, response);
     }
 }
