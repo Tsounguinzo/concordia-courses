8c8beae7880d1dc9307dffe7a30ff3cf6eb2fb4b
Beaudelaire Tsoungui Nzodoumkouo <beaudelaire@tutamail.com>
2024-08-31 08:35:31 -0400

    updated search

--- ui/src/routes/search/+page.svelte
+++ ui/src/routes/search/+page.svelte
@@ -1,33 +1,20 @@
 <script lang="ts">
-    import {createQuery} from '@tanstack/svelte-query';
     import {writable} from 'svelte/store';
     import {repo} from '$lib/repo';
-    import {ArrowRight} from "lucide-svelte";
-    import type {Review} from "$lib/model/Review";
     import {goto} from "$app/navigation";
-    import {afterUpdate, onMount} from "svelte";
+    import ReviewComponent from "$lib/components/review/Review.svelte";
+    import type {Review} from "$lib/model/Review";
+
 
     let input = '';
     const isSearching = writable(false);
     const showResults = writable(false);
 
-    const searchQuery = createQuery({
-        queryKey: ['search', input],
-        queryFn: async () => {
-            if (input.trim() === '') return [];
-            return repo.getReviewsByFTS(input)
-        }
-    });
-
-    function handleSearch() {
-
-    }
-
     function handleInput(event) {
         input = event.target.value;
     }
 
-    $: isEmptySearch = !input && !$searchQuery.isLoading && !$searchQuery.data;
+    $: isEmptySearch = true
 
 
     let reviews: Review[] = [];
@@ -35,25 +22,19 @@
 
     let showEmptyScreen: boolean = false;
     let isLoading: boolean = false;
-    let inputRef: HTMLTextAreaElement;
     let isFirstRender: boolean = true;
 
     $: if (isFirstRender && query && query.trim().length > 0) {
-        handleQuerySubmit(query);
         isFirstRender = false;
     }
 
-    async function handleQuerySubmit(query: string) {
-        input = query;
+    async function handleSubmit() {
         isLoading = true;
 
-        const queryResults = await repo.getReviewsByFTS(query);
+        const queryResults = await repo.getReviewsByFTS(input);
         reviews = [...reviews, ...queryResults];
-    }
 
-    function handleSubmit(e: Event) {
-        e.preventDefault();
-        handleQuerySubmit(input);
+        input = '';
     }
 
     function handleClear() {
@@ -63,39 +44,35 @@
         goto('/search');
     }
 
-    onMount(() => {
-        inputRef.focus();
-    });
-
-    function handleHeightChange(height: number) {
-        if (!inputRef) return;
-
-        const initialHeight = 70;
-        const initialBorder = 32;
-        const multiple = (height - initialHeight) / 20;
-
-        const newBorder = initialBorder - 4 * multiple;
-        inputRef.style.borderRadius = `${Math.max(8, newBorder)}px`;
-    }
 </script>
 
+<div>
+    {#if reviews}
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {#each reviews as review (review._id)}
+                <ReviewComponent {review} />
+            {/each}
+        </div>
+    {/if}
+</div>
 <div class={`transition-all ease-in-out duration-300 flex flex-col items-center ${isEmptySearch ? 'justify-center' : 'justify-end'} min-h-[calc(100vh-19vh)]`}>
     {#if isEmptySearch}
         <h1 class="text-4xl font-bold mb-2 text-gray-800 dark:text-gray-300">Discover Smarter Search</h1>
         <p class="mb-8 text-lg font-semibold text-gray-500 dark:text-gray-400">Easily find reviews you like with our search tool.</p>
     {/if}
-    <form on:submit|preventDefault={handleSearch} class="w-full max-w-2xl mx-auto">
+    <form on:submit={handleSubmit} class="w-full max-w-2xl mx-auto">
         <div class="relative flex items-center">
             <input
+                    autofocus
                     type="text"
                     bind:value={input}
                     on:input={handleInput}
                     placeholder="Search reviews by content..."
-                    class="w-full py-3 px-4 pr-12 rounded-full bg-gray-100 text-gray-900 placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                    class="w-full py-3 px-4 pr-12 rounded-full bg-neutral-200 dark:bg-neutral-800 text-gray-900 dark:text-gray-50 placeholder-gray-500 focus:outline-none focus:ring-2 dark:focus:ring-gray-200 focus:ring-gray-700"
             />
             <button
                     type="submit"
-                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-600"
+                    class="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-400 hover:text-gray-60 dark:hover:text-gray-200"
                     aria-label="Submit search"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
@@ -105,23 +82,3 @@
         </div>
     </form>
 </div>
-
-{#if $searchQuery.data && $searchQuery.data?.length > 0}
-    <div class="results">
-        <ul>
-            {#each $searchQuery.data as result}
-                <li>{result}</li>
-            {/each}
-        </ul>
-    </div>
-{:else if $searchQuery.data && !$searchQuery.isLoading && $searchQuery.data?.length === 0}
-    <div class="results">
-        <p>No results found.</p>
-    </div>
-{/if}
-
-{#if $searchQuery.isLoading}
-    <div class="mt-2">
-        <p>Searching...</p>
-    </div>
-{/if}
