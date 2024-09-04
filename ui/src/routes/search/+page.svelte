ec64ca063e90d0e235170857e66a9a61a09c62ad
Beaudelaire Tsoungui Nzodoumkouo <beaudelaire@tutamail.com>
2024-09-04 13:55:38 -0400

    updated search

--- ui/src/routes/search/+page.svelte
+++ ui/src/routes/search/+page.svelte
@@ -1,64 +1,49 @@
 <script lang="ts">
-    import {writable} from 'svelte/store';
     import {repo} from '$lib/repo';
-    import {goto} from "$app/navigation";
     import ReviewComponent from "$lib/components/review/Review.svelte";
     import type {Review} from "$lib/model/Review";
-
+    import {mode} from "mode-watcher";
+    import Skeleton from "$lib/components/common/loader/Skeleton.svelte";
 
     let input = '';
-    const isSearching = writable(false);
-    const showResults = writable(false);
-
-    function handleInput(event) {
-        input = event.target.value;
-    }
-
-    $: isEmptySearch = true
-
-
-    let reviews: Review[] = [];
-    let query: string = '';
-
-    let showEmptyScreen: boolean = false;
+    let isEmptySearch = true
     let isLoading: boolean = false;
-    let isFirstRender: boolean = true;
-
-    $: if (isFirstRender && query && query.trim().length > 0) {
-        isFirstRender = false;
-    }
+    let reviews: Review[] = [];
 
     async function handleSubmit() {
         isLoading = true;
+        isEmptySearch = false;
 
         const queryResults = await repo.getReviewsByFTS(input);
         reviews = [...reviews, ...queryResults];
 
         input = '';
-    }
-
-    function handleClear() {
         isLoading = false;
-        reviews = [];
-        input = '';
-        goto('/search');
     }
 
 </script>
 
-<div>
-    {#if reviews}
-        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-            {#each reviews as review (review._id)}
-                <ReviewComponent {review} />
-            {/each}
-        </div>
-    {/if}
-</div>
 <div class={`transition-all ease-in-out duration-300 flex flex-col items-center ${isEmptySearch ? 'justify-center' : 'justify-end'} min-h-[calc(100vh-19vh)]`}>
+    <div class="my-10">
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {#if isLoading}
+                <Skeleton class='mb-2 rounded-lg' width={800} height={200}
+                          color={$mode === 'dark' ? 'rgb(38 38 38)' : 'rgb(226 232 240)'}/>
+            {:else}
+                {#key reviews.length}
+                    {#if reviews}
+                        {#each reviews as review (review._id)}
+                            <ReviewComponent {review}/>
+                        {/each}
+                    {/if}
+                {/key}
+            {/if}
+        </div>
+    </div>
     {#if isEmptySearch}
         <h1 class="text-4xl font-bold mb-2 text-gray-800 dark:text-gray-300">Discover Smarter Search</h1>
-        <p class="mb-8 text-lg font-semibold text-gray-500 dark:text-gray-400">Easily find reviews you like with our search tool.</p>
+        <p class="mb-8 text-lg font-semibold text-gray-500 dark:text-gray-400">Easily find reviews you like with our
+            search tool.</p>
     {/if}
     <form on:submit={handleSubmit} class="w-full max-w-2xl mx-auto">
         <div class="relative flex items-center">
@@ -66,7 +51,6 @@
                     autofocus
                     type="text"
                     bind:value={input}
-                    on:input={handleInput}
                     placeholder="Search reviews by content..."
                     class="w-full py-3 px-4 pr-12 rounded-full bg-neutral-200 dark:bg-neutral-800 text-gray-900 dark:text-gray-50 placeholder-gray-500 focus:outline-none focus:ring-2 dark:focus:ring-gray-200 focus:ring-gray-700"
             />
@@ -76,7 +60,9 @@
                     aria-label="Submit search"
             >
                 <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
+                    <path fill-rule="evenodd"
+                          d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                          clip-rule="evenodd"/>
                 </svg>
             </button>
         </div>
